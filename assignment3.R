447fc300c355b24d9ebb154c40c0503235f8881b
mseewaters <mseewaters@gmail.com>
2014-09-18 12:48:31 -0400

    added sorted matching and matching matching

--- assignment3.R
+++ assignment3.R
@@ -17,14 +17,18 @@ colnames(t1) <- c("id","num_follows")
 # Creating a table of users with followers and all attributes
 t2 <- as.data.frame(merge(t1, all, by = "id"))
 
+
 create.formatch <- function(h) 
 {
   # Calculate mean timestamp for each hashtag
   tm <- median(h$timeStamp)
   
+  # Create table of users with last tweet after tm
+  t3 <- t2[t2$timeStamp > tm,]
+    
   # Create vectors with initial setting as FALSE
-  treated = rep(FALSE, times = nrow(t2))
-  adopted = rep(FALSE, times = nrow(t2))
+  treated = rep(FALSE, times = nrow(t3))
+  adopted = rep(FALSE, times = nrow(t3))
 
   # A user is treated if
   # user tt > tm  (the user has tweeted something after the "treatment" period)
@@ -40,11 +44,10 @@ create.formatch <- function(h)
   #   to the same large number
   # This converts an empty set to something useful for the logical criteria
   
-  for (i in 1:nrow(t2))
+  for (i in 1:nrow(t3))
   {
     
-    tt <- all[all$id == t2$id[i], "timeStamp"]
-    th <- h[h$id == t2$id[i], "timeStamp"]
+    th <- h[h$id == t3$id[i], "timeStamp"]
     if (length(th) != 0) adopted[i] = TRUE
     if (length(th) == 0) th = 1000
     
@@ -53,13 +56,13 @@ create.formatch <- function(h)
     temp3 <- subset(merge(temp,h), timeStamp<tm)
     min.fth <- min(temp3$timeStamp, 1000)
     
-    if ((tt > tm) & (th > min.fth)) treated[i] = TRUE
+    if (th > min.fth) treated[i] = TRUE
   
   }
   
   # Create final table
-  t.h <- cbind(treated, t2[,4:7])
-  t.h2 <- cbind(treated, t2)
+  t.h <- cbind(treated, t3[,4:7])
+  t.h2 <- cbind(treated, t3)
   
   # Run logit to predict probability of being treated based on attributes
   model.h <- glm(treated ~ ., data=t.h, family=binomial(link="logit"))
@@ -81,13 +84,18 @@ randomMatch <- function(y,l)
   return(round(mean(h.random),0))
 }
 
-ratioCalc <- function(z)
+
+ratioCalcSort <- function(z)
 {
   ratio = NULL
-  match <- matchit(treated ~ pred.h, data = z, method='nearest')
-  summary(match)
-  match.treat <- match.data(match, group = "treat")
-  match.control <- match.data(match, group = "control")
+  
+  #Using sorted lists
+  match.treat <- subset(z, treated==TRUE)
+  match.treat <- match.treat[order(match.treat$pred.h),]
+  match.control <- subset(z, treated==FALSE)
+  match.control <- match.control[order(match.control$pred.h),]
+  match.control <- match.control[1:nrow(match.treat),]
+
   ratio[1] <- sum(match.treat$adopted)
   ratio[2] <- sum(match.control$adopted)
   ratio[3] <- randomMatch(z, nrow(match.treat))
@@ -96,30 +104,65 @@ ratioCalc <- function(z)
   return(ratio)
 }
 
-library(MatchIt)
+ratioCalcMatch <- function(z)
+{
+  ratio = NULL
+  
+  #Using score matching  
+  match.treat <- subset(z, treated==TRUE)
+  match.treat <- match.treat[order(match.treat$pred.h),]
+  match.untreat <- subset(z, treated==FALSE)
+  match.control <- match.treat  
+  for (i in 1:nrow(match.treat))
+  {
+    x <- match.treat[i,10]
+    ind <- min(which(abs(match.untreat$pred.h - x)==min(abs(match.untreat$pred.h - x))))
+    match.control[i,] <- match.untreat[ind,]
+  }
+  
+  ratio[1] <- sum(match.treat$adopted)
+  ratio[2] <- sum(match.control$adopted)
+  ratio[3] <- randomMatch(z, nrow(match.treat))
+  ratio[4] <- round(ratio[1]/ratio[2],2)
+  ratio[5] <- round(ratio[1]/ratio[3],2)
+  return(ratio)
+}
+
+
 
 # WorldCup
 match.data.wc <- create.formatch(wc)
 xtabs(~adopted+treated, data = match.data.wc)
-ratio.wc <- ratioCalc(match.data.wc)
+ratios.wc <- ratioCalcSort(match.data.wc)
+ratiom.wc <- ratioCalcMatch(match.data.wc)
 
 # Selfie
 match.data.selfie <- create.formatch(selfie)
 xtabs(~adopted+treated, data = match.data.selfie)
-ratio.selfie <- ratioCalc(match.data.selfie)
+ratios.selfie <- ratioCalcSort(match.data.selfie)
+ratiom.selfie <- ratioCalcMatch(match.data.selfie)
 
 # Love
 match.data.love <- create.formatch(love)
 xtabs(~adopted+treated, data = match.data.love)
-ratio.love <- ratioCalc(match.data.love)
+ratios.love <- ratioCalcSort(match.data.love)
+ratiom.love <- ratioCalcMatch(match.data.love)
 
 # tbt
 match.data.tbt <- create.formatch(tbt)
 xtabs(~adopted+treated, data = match.data.tbt)
-ratio.tbt <- ratioCalc(match.data.tbt)
+ratios.tbt <- ratioCalcSort(match.data.tbt)
+ratiom.tbt <- ratioCalcMatch(match.data.tbt)
 
 # Clean and combine
-ratio.list <- as.data.frame(rbind(ratio.wc, ratio.selfie, ratio.love, ratio.tbt))
-colnames(ratio.list) = c("#Adopters:Treated","#Adopters:Control","#Adopters:Random",
+ratio.s.list <- as.data.frame(rbind(ratios.wc, ratios.selfie, ratios.love, ratios.tbt))
+colnames(ratio.s.list) = c("#Adopters:Treated","#Adopters:Control","#Adopters:Random",
                          "Ratio Treated/Control","Ratio Treated/Random")
-ratio.list$hashtag = c("#worldcup","#selfie","#love","#tbt")
+ratio.s.list$hashtag = c("#worldcup","#selfie","#love","#tbt")
+ratio.s.list$treated = c(sum(match.data.wc$treated), sum(match.data.selfie$treated), sum(match.data.love$treated), sum(match.data.tbt$treated))
+
+ratio.m.list <- as.data.frame(rbind(ratiom.wc, ratiom.selfie, ratiom.love, ratiom.tbt))
+colnames(ratio.m.list) = c("#Adopters:Treated","#Adopters:Control","#Adopters:Random",
+                         "Ratio Treated/Control","Ratio Treated/Random")
+ratio.m.list$hashtag = c("#worldcup","#selfie","#love","#tbt")
+ratio.m.list$treated = c(sum(match.data.wc$treated), sum(match.data.selfie$treated), sum(match.data.love$treated), sum(match.data.tbt$treated))
